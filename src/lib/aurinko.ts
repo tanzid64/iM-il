d1811445d57753a96bad871590c6a48b40f7eec0
tanzid64 <tanzid@inbox.ru>
2024-10-08 16:37:32 +0600

    feat: types: EmailMessage, EmailAddress, EmailAttachment, EmailHeader & getEmailDetails function

--- src/lib/aurinko.ts
+++ src/lib/aurinko.ts
@@ -3,6 +3,7 @@
 import { db } from "@/server/db";
 import axios from "axios";
 import { getAuthUserID } from "./user";
+import { EmailMessage } from "./types";
 
 export const getAurinkoAuthorizationUrl = async (
   serviceType: "Google" | "Office365",
@@ -102,3 +103,26 @@ export const getAccountDetails = async (accessToken: string) => {
   }
 };
 
+export const getEmailDetails = async (accessToken: string, emailId: string) => {
+  try {
+    const response = await axios.get<EmailMessage>(
+      `https://api.aurinko.io/v1/email/messages/${emailId}`,
+      {
+        params: {
+          loadInlines: true,
+        },
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Error fetching email details:", error.response?.data);
+    } else {
+      console.error("Unexpected error fetching email details:", error);
+    }
+    throw error;
+  }
+};
---
+++ src/lib/types.ts
@@ -0,0 +1,66 @@
+export interface EmailMessage {
+  id: string;
+  threadId: string;
+  createdTime: string;
+  lastModifiedTime: string;
+  sentAt: string;
+  receivedAt: string;
+  internetMessageId: string;
+  subject: string;
+  sysLabels: Array<
+    | "junk"
+    | "trash"
+    | "sent"
+    | "inbox"
+    | "unread"
+    | "flagged"
+    | "important"
+    | "draft"
+  >;
+  keywords: string[];
+  sysClassifications: Array<
+    "personal" | "social" | "promotions" | "updates" | "forums"
+  >;
+  sensitivity: "normal" | "private" | "personal" | "confidential";
+  meetingMessageMethod?: "request" | "reply" | "cancel" | "counter" | "other";
+  from: EmailAddress;
+  to: EmailAddress[];
+  cc: EmailAddress[];
+  bcc: EmailAddress[];
+  replyTo: EmailAddress[];
+  hasAttachments: boolean;
+  body?: string;
+  bodySnippet?: string;
+  attachments: EmailAttachment[];
+  inReplyTo?: string;
+  references?: string;
+  threadIndex?: string;
+  internetHeaders: EmailHeader[];
+  nativeProperties: Record<string, string>;
+  folderId?: string;
+  omitted: Array<
+    "threadId" | "body" | "attachments" | "recipients" | "internetHeaders"
+  >;
+}
+
+export interface EmailAddress {
+  name?: string;
+  address: string;
+  raw?: string;
+}
+
+export interface EmailAttachment {
+  id: string;
+  name: string;
+  mimeType: string;
+  size: number;
+  inline: boolean;
+  contentId?: string;
+  content?: string;
+  contentLocation?: string;
+}
+
+export interface EmailHeader {
+  name: string;
+  value: string;
+}
